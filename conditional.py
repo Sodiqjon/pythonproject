574066a6d29a5c99718f7ff787b29ce87e370e84
root <root@serverb.ziyotek.local>
2020-04-23 02:33:26 -0400

    initial commit

---
+++ conditional.py
@@ -0,0 +1,6 @@
+likes_pineapple_on_pizza = True
+
+if(not likes_pineapple_on_pizza):
+    print("You areweird!")
+else:
+(likes_pineapple-on_pizza == True):
---
+++ game.py
@@ -0,0 +1,9 @@
+# Author:  Sodiqjon Mumin
+# Contact: sodiqjon1978@gmail.com
+# A fun little python game
+
+name = input("What is your name?")
+age = input("What is your age?")
+
+print("Hi, " + name + "!")
+print("Your age is " + str(age))
